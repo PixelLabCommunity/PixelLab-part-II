f930286ae5c3fab08875ac88a1607843ba540a45
cooler-SAI <cooler19860212@yahoo.com>
2023-10-18 21:11:02 +0300

    Unity/Lab-3D: added new script to repo

---
+++ Unity Development/Learning/Lab-3D/Assets/Scripts/HeroesNames.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class HeroesNames : MonoBehaviour
+{
+    public string firstPlayer = "Alex!";
+    public string secondPlayer = "Lena!";
+}
---
+++ Unity Development/Learning/Lab-3D/Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class ScoreScript : MonoBehaviour
+{
+    public int scoreResult = 9005;
+    public int scoreResultFail = 205;
+}
