3f887c0039b51cc22d06a9cd6be8327a089a85fe
cooler-SAI <cooler19860212@yahoo.com>
2023-10-19 12:13:12 +0300

    Unity/Lab-3D: update movement script

--- Unity Development/Learning/Lab-3D/Assets/Scripts/TimeDeltaTime.cs
+++ Unity Development/Learning/Lab-3D/Assets/Scripts/TimeDeltaTime.cs
@@ -6,14 +6,21 @@ public class TimeDeltaTime : MonoBehaviour
     [SerializeField] private float countdown = 3f;
     private readonly float _zeroValue = 0f;
 
-    private void Update()
+    private void FixedUpdate()
     {
         CountdownLight();
+        SmoothMovement();
     }
 
     private void CountdownLight()
     {
         countdown -= Time.deltaTime;
         if (countdown <= _zeroValue) GetComponent<Light>().enabled = true;
+    }
+
+    private void SmoothMovement()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            transform.position += new Vector3(speed * Time.deltaTime, _zeroValue, _zeroValue);
     }
 }
