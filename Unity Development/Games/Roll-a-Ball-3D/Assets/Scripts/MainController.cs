fad1f9b5555a57afca0607aaa7e07a53265eaa9f
cooler-SAI <cooler19860212@yahoo.com>
2023-10-26 23:55:52 +0300

    Unity/Roll-a-Ball-3D: quick update

--- Unity Development/Games/Roll-a-Ball-3D/Assets/Scripts/MainController.cs
+++ Unity Development/Games/Roll-a-Ball-3D/Assets/Scripts/MainController.cs
@@ -4,10 +4,10 @@ using UnityEngine.InputSystem;
 public class MainController : MonoBehaviour
 {
     [SerializeField] private float movementSpeed = 10;
-    private Rigidbody _rigidbody;
+    private readonly float _movementBase = 0.0f;
     private float _movementVectorX;
     private float _movementVectorY;
-    private readonly float _movementBase = 0.0f;
+    private Rigidbody _rigidbody;
 
     private void Start()
     {
@@ -16,16 +16,14 @@ public class MainController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Vector3 movement = new Vector3(_movementVectorX, _movementBase, _movementVectorY);
-        _rigidbody.AddForce(movement*movementSpeed);
+        var movement = new Vector3(_movementVectorX, _movementBase, _movementVectorY);
+        _rigidbody.AddForce(movement * movementSpeed);
     }
-    
+
     private void OnMove(InputValue movementValue)
     {
-        Vector2 movementVector = movementValue.Get<Vector2>();
+        var movementVector = movementValue.Get<Vector2>();
         _movementVectorX = movementVector.x;
         _movementVectorY = movementVector.y;
     }
-
-   
 }
