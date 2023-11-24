9f2e826613c15d2478a35327e6d0b2cac9bb578e
cooler-SAI <cooler19860212@yahoo.com>
2023-11-25 01:10:54 +0300

    Godot/Games/Ninja-2D: added animation

--- Godot Development/Games/Ninja-2D/Scripts/character_body_2d.cs
+++ Godot Development/Games/Ninja-2D/Scripts/character_body_2d.cs
@@ -3,36 +3,42 @@ using System;
 
 public partial class character_body_2d : CharacterBody2D
 {
-	public const float Speed = 50.0f;
-	public const float JumpVelocity = -100.0f;
+	private const float Speed = 50.0f;
+	private AnimationPlayer _animationPlayer;
 
-	// Get the gravity from the project settings to be synced with RigidBody nodes.
-	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+	public override void _Ready()
+	{
+		_animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+	}
 
 	public override void _PhysicsProcess(double delta)
 	{
-		Vector2 velocity = Velocity;
+		var velocity = Velocity;
+		var direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
 
-		// Add the gravity.
-		/*if (!IsOnFloor())
-			velocity.Y += gravity * (float)delta;*/
-
-		// Handle Jump.
-		/*if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-			velocity.Y = JumpVelocity;*/
-
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
 		if (direction != Vector2.Zero)
 		{
 			velocity.X = direction.X * Speed;
 			velocity.Y = direction.Y * Speed;
+
+			// Determine the animation to play based on the movement direction
+			if (direction.X < 0)
+				_animationPlayer.Play("walkLeft");
+			else if (direction.X > 0)
+				_animationPlayer.Play("walkRight");
+
+			if (direction.Y < 0)
+				_animationPlayer.Play("walkUp");
+			else if (direction.Y > 0)
+				_animationPlayer.Play("walkDown");
 		}
 		else
 		{
 			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-			velocity.Y = Mathf.MoveToward(0, Velocity.Y, Speed);
+			velocity.Y = Mathf.MoveToward(Velocity.Y, 0, Speed);
+
+			// If no movement, stop any playing animation
+			_animationPlayer.Stop();
 		}
 
 		Velocity = velocity;
