7323724b0ad29997b71467419209b0df8f487fdc
cooler-SAI <cooler19860212@yahoo.com>
2023-12-20 22:44:30 +0300

    Godot/Games/Ninja-2D: update cam script, more misc updates

--- Godot Development/Games/Ninja-2D/Scripts/Camera2D.cs
+++ Godot Development/Games/Ninja-2D/Scripts/Camera2D.cs
@@ -1,8 +1,10 @@
 using Godot;
 
+namespace Ninja2D.Scripts;
+
 public partial class Camera2D : Godot.Camera2D
 {
-	[Export] public TileMap Tilemap { get; set; }
+	[Export] private TileMap Tilemap { get; set; }
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -12,16 +14,12 @@ public partial class Camera2D : Godot.Camera2D
 		// Check if CellQuadrantSize is Vector2
 		var tileSize = Tilemap.CellQuadrantSize;
 
-		var worldSizeInPixels = mapRect.Size * tileSize;
+		var (worldWight, worldHeight) = mapRect.Size * tileSize;
 
-		// Assign float values directly to LimitRight and LimitBottom
-		LimitRight = (int)worldSizeInPixels.X;
-		LimitBottom = (int)worldSizeInPixels.Y;
-	}
+		LimitRight = (int)worldWight;
+		LimitLeft = -(int)worldWight;
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public void _Process(float delta)
-	{
-		// Your _process code goes here
+		LimitTop = -(int)worldHeight;
+		LimitBottom = (int)worldHeight;
 	}
 }
