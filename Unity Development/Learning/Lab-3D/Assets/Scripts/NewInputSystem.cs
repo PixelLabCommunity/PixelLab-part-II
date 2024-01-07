065244663f0e785b12fc14af24917d0d3b365554
cooler-SAI <cooler19860212@yahoo.com>
2024-01-08 01:20:36 +0300

    Unity/Learning/Lab-3D: few things added, clearn up the code

--- Unity Development/Learning/Lab-3D/Assets/Scripts/NewInputSystem.cs
+++ Unity Development/Learning/Lab-3D/Assets/Scripts/NewInputSystem.cs
@@ -3,47 +3,16 @@ using UnityEngine.InputSystem;
 
 public class NewInputSystem : MonoBehaviour
 {
-    [SerializeField] private InputAction playerControls;
-    [SerializeField] private float movementSpeed = 10.0f;
-    [SerializeField] private float jumpForce = 15.0f;
-    private readonly float _basePositionY = 0.0f;
+    [SerializeField] private float speed;
     private Rigidbody _rigidbody;
-    private Vector3 _spawnPosition = Vector3.zero;
 
     private void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
     }
 
-    private void Update()
+    private void OnMove(InputValue inputValue)
     {
-        _spawnPosition = playerControls.ReadValue<Vector3>();
-        JumpButton();
-    }
-
-    private void FixedUpdate()
-    {
-        _rigidbody.velocity = new Vector3(_spawnPosition.x * movementSpeed, _basePositionY,
-            _spawnPosition.z * movementSpeed);
-    }
-
-    private void OnEnable()
-    {
-        playerControls.Enable();
-    }
-
-    private void OnDisable()
-    {
-        playerControls.Disable();
-    }
-
-    private void Jump()
-    {
-        _rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-    }
-
-    private void JumpButton()
-    {
-        if (Input.GetKeyDown(KeyCode.Space)) Jump();
+        _rigidbody.velocity = inputValue.Get<Vector3>() * speed;
     }
 }
