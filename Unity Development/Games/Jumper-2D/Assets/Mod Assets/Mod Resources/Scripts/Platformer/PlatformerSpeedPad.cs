32e7b0bb357dbb832dcef18c40d98a1872ce38ff
cooler-SAI <cooler19860212@yahoo.com>
2024-01-15 20:59:12 +0300

    Unity/Games/Jumper-2D: update jump effect, added boosters

--- Unity Development/Games/Jumper-2D/Assets/Mod Assets/Mod Resources/Scripts/Platformer/PlatformerSpeedPad.cs
+++ Unity Development/Games/Jumper-2D/Assets/Mod Assets/Mod Resources/Scripts/Platformer/PlatformerSpeedPad.cs
@@ -1,15 +1,15 @@
 ﻿using System.Collections;
-using UnityEngine;
 using Platformer.Mechanics;
+using UnityEngine;
 
 public class PlatformerSpeedPad : MonoBehaviour
 {
     public float maxSpeed;
 
-    [Range (0, 5)]
-    public float duration = 1f;
+    [Range(0, 5)] public float duration = 1f;
 
-    void OnTriggerEnter2D(Collider2D other){
+    private void OnTriggerEnter2D(Collider2D other)
+    {
         var rb = other.attachedRigidbody;
         if (rb == null) return;
         var player = rb.GetComponent<PlayerController>();
@@ -17,11 +17,11 @@ public class PlatformerSpeedPad : MonoBehaviour
         player.StartCoroutine(PlayerModifier(player, duration));
     }
 
-    IEnumerator PlayerModifier(PlayerController player, float lifetime){
+    private IEnumerator PlayerModifier(PlayerController player, float lifetime)
+    {
         var initialSpeed = player.maxSpeed;
         player.maxSpeed = maxSpeed;
         yield return new WaitForSeconds(lifetime);
         player.maxSpeed = initialSpeed;
     }
-
 }
