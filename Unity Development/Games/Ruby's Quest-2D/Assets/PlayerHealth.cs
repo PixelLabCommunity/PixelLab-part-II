dfb84777a36552749aac8690bb4d6bd925985e7f
cooler-SAI <cooler19860212@yahoo.com>
2024-01-25 22:58:22 +0300

    Unity/Games/Ruby's Quest-2D: update scripts, added health to the player

---
+++ Unity Development/Games/Ruby's Quest-2D/Assets/PlayerHealth.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    private const int MinHealth = 0;
+    private const int HealthUp = 1;
+    [SerializeField] private int maxHealth = 5;
+
+    private int _currentHealth;
+
+    private void Start()
+    {
+        _currentHealth = maxHealth;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.CompareTag("HealthUp")) return;
+        _currentHealth += HealthUp;
+        Destroy(other.gameObject);
+    }
+
+    private void ChangeHealth(int amount)
+    {
+        _currentHealth = Mathf.Clamp(_currentHealth + amount, MinHealth, maxHealth);
+        Debug.Log(_currentHealth + "/" + maxHealth);
+    }
+}
