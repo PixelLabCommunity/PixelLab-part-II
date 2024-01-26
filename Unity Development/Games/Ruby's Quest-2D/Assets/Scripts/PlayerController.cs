641bd416c79145986258221bcba4d732efa146c8
cooler-SAI <cooler19860212@yahoo.com>
2024-01-26 22:02:48 +0300

    Unity/Games/Ruby's Quest-2D: added animation for the player, misc fixes and updates

--- Unity Development/Games/Ruby's Quest-2D/Assets/Scripts/PlayerController.cs
+++ Unity Development/Games/Ruby's Quest-2D/Assets/Scripts/PlayerController.cs
@@ -3,17 +3,34 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    private static readonly int Property = Animator.StringToHash("Look X");
+    private static readonly int Property1 = Animator.StringToHash("Look Y");
+    private static readonly int Speed = Animator.StringToHash("Speed");
     [SerializeField] private float speed = 5.0f;
+    private Animator _animator;
+    private Vector2 _lookDirection = new(1, 0);
 
     private Rigidbody2D _rigidbody2D;
 
     private void Awake()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
 
     private void OnMove(InputValue inputValue)
     {
-        _rigidbody2D.velocity = inputValue.Get<Vector2>() * speed;
+        var move = inputValue.Get<Vector2>();
+        _rigidbody2D.velocity = move * speed;
+
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            _lookDirection.Set(move.x, move.y);
+            _lookDirection.Normalize();
+        }
+
+        _animator.SetFloat(Property, _lookDirection.x);
+        _animator.SetFloat(Property1, _lookDirection.y);
+        _animator.SetFloat(Speed, move.magnitude);
     }
 }
