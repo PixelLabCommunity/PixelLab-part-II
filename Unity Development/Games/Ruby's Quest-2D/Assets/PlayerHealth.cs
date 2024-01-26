f4e354dad9a3599890e3842672114e4738a03e71
cooler-SAI <cooler19860212@yahoo.com>
2024-01-26 21:12:18 +0300

    Unity/Game/Ruby's Quest-2D: added new damage Gameobject, misc updates and fixes

--- Unity Development/Games/Ruby's Quest-2D/Assets/PlayerHealth.cs
+++ Unity Development/Games/Ruby's Quest-2D/Assets/PlayerHealth.cs
@@ -4,13 +4,24 @@ public class PlayerHealth : MonoBehaviour
 {
     private const int MinHealth = 0;
     private const int HealthUp = 1;
+    private const int HealthDown = -1;
     [SerializeField] private int maxHealth = 5;
+    private readonly float _timeInvisible = 2.0f;
 
     private int _currentHealth;
+    private float _invisibleTimer;
+    private bool _isInvisible;
 
     private void Start()
     {
         _currentHealth = maxHealth;
+    }
+
+    private void Update()
+    {
+        if (!_isInvisible) return;
+        _invisibleTimer -= Time.deltaTime;
+        if (_invisibleTimer < 0) _isInvisible = false;
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -18,10 +29,23 @@ public class PlayerHealth : MonoBehaviour
         if (!other.CompareTag("HealthUp")) return;
         ChangeHealth(HealthUp);
         Destroy(other.gameObject);
+        Debug.Log("Your Health increased to 1 HP! ");
+        if (!other.CompareTag("HealthDown")) return;
+        ChangeHealth(HealthDown);
+        Debug.Log("Your Health decreased to -1 HP! ");
     }
 
-    private void ChangeHealth(int amount)
+    public void ChangeHealth(int amount)
     {
+        if (amount < 0)
+        {
+            if (_isInvisible)
+                return;
+
+            _isInvisible = true;
+            _invisibleTimer = _timeInvisible;
+        }
+
         _currentHealth = Mathf.Clamp(_currentHealth + amount, MinHealth, maxHealth);
         Debug.Log(_currentHealth + "/" + maxHealth);
     }
--- Unity Development/Games/Ruby's Quest-2D/Assets/UnityTechnologies/Scripts/HealthCollectible.cs
+++ Unity Development/Games/Ruby's Quest-2D/Assets/UnityTechnologies/Scripts/HealthCollectible.cs
@@ -1,18 +1,12 @@
 ﻿using UnityEngine;
 
-/// <summary>
-/// Will handle giving health to the character when they enter the trigger.
-/// </summary>
-public class HealthCollectible : MonoBehaviour 
+public class HealthCollectible : MonoBehaviour
 {
-    void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        RubyController controller = other.GetComponent<RubyController>();
+        var controller = other.GetComponent<PlayerHealth>();
 
-        if (controller != null)
-        {
-            controller.ChangeHealth(1);
-            Destroy(gameObject);
-        }
+        if (controller != null) controller.ChangeHealth(1);
+        //Debug.Log("Your health increased to 1 !");
     }
 }
