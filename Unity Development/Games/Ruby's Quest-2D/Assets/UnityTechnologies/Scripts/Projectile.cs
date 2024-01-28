01caf886e3d30e21e7c77e3f2fc4b3e28548c382
cooler-SAI <cooler19860212@yahoo.com>
2024-01-28 20:31:37 +0300

    Unity/Games/Ruby's Quest-2D: added fisics, misc new updates

--- Unity Development/Games/Ruby's Quest-2D/Assets/UnityTechnologies/Scripts/Projectile.cs
+++ Unity Development/Games/Ruby's Quest-2D/Assets/UnityTechnologies/Scripts/Projectile.cs
@@ -1,40 +1,31 @@
 ﻿using UnityEngine;
 
-/// <summary>
-/// Handle the projectile launched by the player to fix the robots.
-/// </summary>
 public class Projectile : MonoBehaviour
 {
-    Rigidbody2D rigidbody2d;
-    
-    void Awake()
+    private Rigidbody2D _rigidbody2d;
+
+    private void Awake()
     {
-        rigidbody2d = GetComponent<Rigidbody2D>();
+        _rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
-    void Update()
+    private void Update()
     {
-        //destroy the projectile when it reach a distance of 1000.0f from the origin
-        if(transform.position.magnitude > 1000.0f)
+        if (transform.position.magnitude > 1000.0f)
             Destroy(gameObject);
     }
 
-    //called by the player controller after it instantiate a new projectile to launch it.
-    public void Launch(Vector2 direction, float force)
+    private void OnCollisionEnter2D(Collision2D other)
     {
-        rigidbody2d.AddForce(direction * force);
+        var enemy = other.collider.GetComponent<Enemy>();
+
+        if (enemy != null) enemy.Fix();
+
+        Destroy(gameObject);
     }
 
-    void OnCollisionEnter2D(Collision2D other)
+    public void Launch(Vector2 direction, float force)
     {
-        Enemy e = other.collider.GetComponent<Enemy>();
-
-        //if the object we touched wasn't an enemy, just destroy the projectile.
-        if (e != null)
-        {
-            e.Fix();
-        }
-        
-        Destroy(gameObject);
+        _rigidbody2d.AddForce(direction * force);
     }
 }
