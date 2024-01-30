3c0569e83856eea7c556f346ecc17f42d83d0476
cooler-SAI <cooler19860212@yahoo.com>
2024-01-30 19:52:33 +0300

    Unity/Games/Ruby's Quest-2D: added new npc, added text, misc updates

--- Unity Development/Games/Ruby's Quest-2D/Assets/Scripts/PlayerController.cs
+++ Unity Development/Games/Ruby's Quest-2D/Assets/Scripts/PlayerController.cs
@@ -18,6 +18,11 @@ public class PlayerController : MonoBehaviour
         _animator = GetComponent<Animator>();
     }
 
+    private void Update()
+    {
+        PlayerTalk();
+    }
+
     private void OnMove(InputValue inputValue)
     {
         var move = inputValue.Get<Vector2>();
@@ -32,5 +37,14 @@ public class PlayerController : MonoBehaviour
         _animator.SetFloat(Property, _lookDirection.x);
         _animator.SetFloat(Property1, _lookDirection.y);
         _animator.SetFloat(Speed, move.magnitude);
+    }
+
+    private void PlayerTalk()
+    {
+        if (!Input.GetKeyDown(KeyCode.X)) return;
+        Vector2 lookDirection = default;
+        var hit = Physics2D.Raycast(_rigidbody2D.position + Vector2.up * 0.2f, lookDirection, 1.5f,
+            LayerMask.GetMask("NPC"));
+        if (hit.collider != null) Debug.Log("Ray-cast has hit the object " + hit.collider.gameObject);
     }
 }
