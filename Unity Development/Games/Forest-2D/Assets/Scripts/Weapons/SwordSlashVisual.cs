333c1d8a6622b38cc932fe7415c01d72f98dc6a0
cooler-SAI <cooler19860212@yahoo.com>
2024-02-16 23:44:35 +0300

    Unity/Games/Forest-2D: misc fixes and updates

---
+++ Unity Development/Games/Forest-2D/Assets/Scripts/Weapons/SwordSlashVisual.cs
@@ -0,0 +1,24 @@
+using System;
+using UnityEngine;
+
+public class SwordSlashVisual : MonoBehaviour
+{
+    private const string ATTACK = "Attack";
+    [SerializeField] private Sword sword;
+    private Animator _animator;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        sword.OnSwordSwing += Sword_OnSwordSwing;
+    }
+
+    private void Sword_OnSwordSwing(object sender, EventArgs e)
+    {
+        _animator.SetTrigger(ATTACK);
+    }
+}
